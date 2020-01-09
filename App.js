bc8c441f6c3fe0317a88cc444de576b45521b07f
Aniket Kumar <aniketkumar049@gmail.com>
2020-01-09 05:52:36 +0530

    Image gallery for local files

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
 import Welcome from './screens/Welcome';
+import Gallery from './screens/Gallery';
 
 var RootScreen = 'Welcome';
 const AppNavigator = createStackNavigator(
   {
     Welcome: Welcome,
+    Gallery: Gallery
   },
   {
     initialRouteName: RootScreen,
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,70 +1,184 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import {
+    StyleSheet,
+    // Text,
+    View,
+    TouchableOpacity,
+    // FlatList,
+    Modal,
+    Image,
+    FlatList
+  } from 'react-native';
 
 import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
 import RNFetchBlob from 'rn-fetch-blob';
 // import RNTextDetector from "react-native-text-detector";
+// import Gallery from './Gallery';
+import FastImage from 'react-native-fast-image';
 
 class Welcome extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            images:[],
+            imageuri: '',
+            ModalVisibleStatus: false,
         };
     }
 
-    componentDidMount = () => {
+    ShowModalFunction(visible, imageURL) {
+        //handler to handle the click on image of Grid
+        //and close button on modal
+        this.setState({
+          ModalVisibleStatus: visible,
+          imageuri: imageURL,
+        });
+      }
+
+
+    renderGallery = () => {
         const dirs = RNFetchBlob.fs.dirs;
         const clutter = dirs.PictureDir.split("Containers/Data");
         const picDir = clutter[0]+'Media/DCIM/100APPLE/';
-        console.log(picDir);
-        // const newFile = picDir+'IMG_0001.JPG';
-        // const visionResp = await RNTextDetector.detectFromUri(newFile);
-        //       console.log('visionResp', visionResp);
-        // detectText = async (newFile) => {
-        //     try {
-        //       const options = {
-        //         quality: 0.8,
-        //         base64: true,
-        //         skipProcessing: true,
-        //       };
-        //     //   const { uri } = await this.camera.takePictureAsync(options);
-        //       const visionResp = await RNTextDetector.detectFromUri(newFile);
-        //       console.log('visionResp', visionResp);
-        //     } catch (e) {
-        //       console.warn(e);
-        //     }
-        //   }
 
-        // RNFetchBlob.fs.ls(picDir).then(files => {
-        //     console.log(files);
-            
-            
-            
-        //   }).catch(error => console.log(error))
+        
+        const imageDataBase = []
 
-        }
+        RNFetchBlob.fs.ls(picDir).then(files => {
+            
+            files.map((f) => {
+                // console.log(picDir+f);
+                // RNFetchBlob.fs.readFile(picDir+f, 'base64')
+                // .then((data) => {
+                // // handle the data ..
+                // var base64Icon = `data:image/jpg;base64,${data}`
+                imageDataBase.push(picDir+f);
+
+                // })
+
+            })
+            console.log("files",imageDataBase );
+            this.setState({images: imageDataBase});
+            
+            
+        }).catch(error => console.log(error))
+
+            return(
+                <FlatList
+                  data={this.state.images}
+                  renderItem={({ item }) => (
+                    <View style={{ flex: 1, flexDirection: 'column', margin: 1 }}>
+                      <TouchableOpacity
+                        key={item.id}
+                        style={{ flex: 1 }}
+                        onPress={() => {
+                          this.ShowModalFunction(true, item);
+                        }}>
+                        <FastImage
+                          style={styles.image}
+                          source={{
+                            uri: item,
+                          }}
+                        />
+                      </TouchableOpacity>
+                    </View>
+                  )}
+                  //Setting the number of column
+                  numColumns={3}
+                  keyExtractor={(item, index) => index.toString()}
+                />
+            )
+
+    }
     
 
     render() {
-        return (
-        <Container>
-            <Header searchBar rounded>
-            <Item>
-                <Icon name="ios-search" />
-                <Input placeholder="Search" />
-            </Item>
-            <Button transparent>
-                <Text>Search</Text>
-            </Button>
-            </Header>
-        </Container>
-        );
+
+        
+        if (this.state.ModalVisibleStatus) {
+            return (
+              <Modal
+                transparent={false}
+                animationType={'fade'}
+                visible={this.state.ModalVisibleStatus}
+                onRequestClose={() => {
+                  this.ShowModalFunction(!this.state.ModalVisibleStatus, '');
+                }}>
+                <View style={styles.modelStyle}>
+                  <FastImage
+                    style={styles.fullImageStyle}
+                    source={{ uri: this.state.imageuri }}
+                    resizeMode={FastImage.resizeMode.contain}
+                  />
+                  <TouchableOpacity
+                    activeOpacity={0.5}
+                    style={styles.closeButtonStyle}
+                    onPress={() => {
+                      this.ShowModalFunction(!this.state.ModalVisibleStatus, '');
+                    }}>
+                    <FastImage
+                      source={{
+                        uri:
+                          'https://raw.githubusercontent.com/AboutReact/sampleresource/master/close.png',
+                      }}
+                      style={{ width: 35, height: 35, marginTop: 16 }}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </Modal>
+            );
+          } else {
+            return (
+              
+            <Container>
+                <Header searchBar rounded>
+                <Item>
+                    <Icon name="ios-search" />
+                    <Input placeholder="Search" />
+                </Item>
+                <Button transparent>
+                    <Text>Search</Text>
+                </Button>
+                </Header>
+                {this.renderGallery()}
+            </Container>
+
+               
+            
+            );
+          }
   }
 }
 
 const styles = StyleSheet.create({
-    
-})
+    container: {
+      flex: 1,
+      marginTop: 30,
+    },
+    image: {
+      height: 120,
+      width: '100%',
+    },
+    fullImageStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%',
+      width: '98%',
+      resizeMode: 'contain',
+    },
+    modelStyle: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0,0,0,0.4)',
+    },
+    closeButtonStyle: {
+      width: 25,
+      height: 25,
+      top: 9,
+      right: 9,
+      position: 'absolute',
+    },
+  });
 
 export default Welcome
